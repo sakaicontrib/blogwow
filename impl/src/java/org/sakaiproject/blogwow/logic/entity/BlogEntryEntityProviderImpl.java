113c9afd70a79df1e27a849a86e1b3ca28e4c1ea
David Horwitz <dhorwitz@gmail.com>
2020-04-23 12:15:03 +0200

    SAK-21839 replacing dead link to osedu.org with link to opensource.org ECL-2.0

--- impl/src/java/org/sakaiproject/blogwow/logic/entity/BlogEntryEntityProviderImpl.java
+++ impl/src/java/org/sakaiproject/blogwow/logic/entity/BlogEntryEntityProviderImpl.java
@@ -1,17 +1,18 @@
-/******************************************************************************
- * BlogEntryEntityProviderImpl.java - created by aaronz on Jun 21, 2007
- * 
- * Copyright (c) 2007 Centre for Academic Research in Educational Technologies
- * Licensed under the Educational Community License version 1.0
- * 
- * A copy of the Educational Community License has been included in this 
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
- * 
- * Contributors:
- * Aaron Zeckoski (aaronz@vt.edu) - primary
- * 
- *****************************************************************************/
-
+/**
+ * Copyright (c) 2003-2020 The Apereo Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://opensource.org/licenses/ecl2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sakaiproject.blogwow.logic.entity;
 
 import java.util.Date;
