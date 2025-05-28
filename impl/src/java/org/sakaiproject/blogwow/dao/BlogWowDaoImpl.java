aa0529623325935250cc1a1cdc4b7d35d568d41f
David Horwitz <dhorwitz@gmail.com>
2025-05-28 19:55:13 +0200

    SAK-21839 replacing dead link to osedu.org with link to opensource.org ECL-2.0 (#22)

--- impl/src/java/org/sakaiproject/blogwow/dao/BlogWowDaoImpl.java
+++ impl/src/java/org/sakaiproject/blogwow/dao/BlogWowDaoImpl.java
@@ -1,14 +1,18 @@
-/******************************************************************************
- * BlogWowDaoImpl.java - created by Sakai App Builder -AZ
- * 
- * Copyright (c) 2006 Sakai Project/Sakai Foundation
- * Licensed under the Educational Community License version 1.0
- * 
- * A copy of the Educational Community License has been included in this 
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
- * 
- *****************************************************************************/
-
+/**
+ * Copyright (c) 2003-2020 The Apereo Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://opensource.org/licenses/ecl2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sakaiproject.blogwow.dao;
 
 import java.util.ArrayList;
